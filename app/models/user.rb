3028d41ec1c405b7e8cac64e9a0bd5534347d75e
Ted Chen <teddy70642000@gmail.com>
2020-07-31 17:05:39 +0800

    密碼加密功能完成 與 使用者路徑建立

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,28 @@
 class User < ApplicationRecord
+    validates :account, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+
+
+    before_create :encrypt_password
+    def self.login(options)
+        if options[:account] && options[:password]
+            find_by( account: options[:account],
+                    password: Digest::SHA1.hexdigest("x"+options[:password]+"y")
+                    # password: bigbang(options[:password]) )
+        else
+            false
+        end
+    end
+
+
+    private
+    def encrypt_password
+        self.password = bigbang(self.password)
+    end
+
+    def bigbang(string)
+        string = "x" + string + "y"
+        Digest::SHA1.hexdigest(string)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,20 @@ Rails.application.routes.draw do
 
   resources :posts, except: [:index, :new, :create]
   # except： 我們不要做出哪些 
+
+  resources :users, only: [ :create ] do
+    # member do   用這個路徑會包含id  我們不要
+    #   get :profile
+    # end
+    collection do
+      get :sign_up 
+      get :edit
+      patch :update
+      get :sign_in 
+      post :login
+      delete :sign_out
+      # get post delete 都可以，這些都是假的，但是logout會有去除 session 的行為，用delete比較符合 restful精神
+    end
+    # 用這個路徑不會有 ID  需要這個
+  end
 end
